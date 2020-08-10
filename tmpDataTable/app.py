6a47c71db38846aca7174dda1efbea206bbaff3a
Jakub Unold <jakub@unold.pl>
2020-08-10 15:59:07 +0100

    Stuck on Intro to Flask-Login

--- tmpDataTable/app.py
+++ tmpDataTable/app.py
@@ -1,8 +1,25 @@
 from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 import requests
 import json
+
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI' = 'sqlite:///login.db']
+app.config['SECRET_KEY'] = 'thisissecret'
+
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(30), unique=True)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 @app.route('/')
